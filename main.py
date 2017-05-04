0ee1744205a2ef21b7bda5cc1f3f8a29d69daaff
Péter Biró <biro.peter.hu@gmail.com>
2017-05-04 21:21:00 +0200

    Plain list without buttons

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 
 class Story_class:
-    def __init__(self, title, long_desc, accept, value, estim, status):
+    def __init__(self, id, title, long_desc, accept, value, estim, status):
+        self.id = id
         self.title = title
         self.long_desc = long_desc
         self.accept = accept
@@ -12,16 +13,35 @@ class Story_class:
         self.status = status
 
 
+def load_stories_from_file(file_name):
+    with open(file_name, "r") as file:
+        lines = file.readlines()
+    story_list = []
+    for element in lines:
+        temp_list = element.replace("\n", "").split(";")
+        story = Story_class(int(temp_list[0]),
+                            temp_list[1],
+                            temp_list[2],
+                            temp_list[3],
+                            int(temp_list[4]),
+                            temp_list[5],
+                            temp_list[6]
+                            )
+        story_list.append(story)
+    return story_list
+
+
 @app.route("/")
 @app.route("/list/")
 def list():
-    return ("This will be the list page")
+    story_list = load_stories_from_file("stories.csv")
+    return render_template("list.html", story_list=story_list)
 
 
 @app.route("/story/", methods=["GET", "POST"])
 def add_new_story():
     if request.method == "GET":
-        story = Story_class(None, None, None, None, None, None)
+        story = Story_class(None, None, None, None, None, None, None)
         return render_template("form.html", title="Add new Story", story=story)
     elif request.method == "POST":
         return ("I am doing it...")
