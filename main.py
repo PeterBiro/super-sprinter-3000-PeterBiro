f5fa40e67ec61d92ea6ce7e5d4fd1343f3f904be
Péter Biró <biro.peter.hu@gmail.com>
2017-05-04 18:27:33 +0200

    forms.html is forming :)

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from flask import Flask
-
+from flask import Flask, request, render_template
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
-    return ("This is the index.html page.")
+    return ("This is the index page. Method used:{}".format(request.method))
+
+
+@app.route("/tuna", methods=["GET", "POST"])
+def tuna():
+    return ("This is the tuna page. Looks awsome! Method used:{}".format(request.method))
+
 
 def main():
     pass
