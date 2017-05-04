3688571d219642d8fe756c1b29a369774fd7fc02
Péter Biró <biro.peter.hu@gmail.com>
2017-05-04 19:19:29 +0200

    form.html is dynamic

--- main.py
+++ main.py
@@ -2,14 +2,29 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 
+class Story_class:
+    def __init__(self, title, long_desc, accept, value, estim, status):
+        self.title = title
+        self.long_desc = long_desc
+        self.accept = accept
+        self.value = value
+        self.estim = estim
+        self.status = status
+
+
 @app.route("/")
-def index():
-    return ("This is the index page. Method used:{}".format(request.method))
+@app.route("/list/")
+def list():
+    return ("This will be the list page")
 
 
-@app.route("/tuna", methods=["GET", "POST"])
-def tuna():
-    return ("This is the tuna page. Looks awsome! Method used:{}".format(request.method))
+@app.route("/story/", methods=["GET", "POST"])
+def add_new_story():
+    if request.method == "GET":
+        story = Story_class(None, None, None, None, None, None)
+        return render_template("form.html", title="Add new Story", story=story)
+    elif request.method == "POST":
+        return ("I am doing it...")
 
 
 def main():
