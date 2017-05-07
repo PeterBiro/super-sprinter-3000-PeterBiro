5f06e755b9bb0c952d5d8ad3ec90e0905bb2d9bd
Péter Biró <biro.peter.hu@gmail.com>
2017-05-07 14:12:26 +0200

    Labels, ids, comments added

--- main.py
+++ main.py
@@ -14,6 +14,10 @@ class Story_class:
 
 
 def load_stories_from_file(file_name):
+    """
+    Load stories from a file & return them in a list of custom class
+    @file_name: string to a text file, where data is separeted by ; and records are in lines
+    """
     with open(file_name, "r") as file:
         lines = file.readlines()
     story_list = []
@@ -32,6 +36,11 @@ def load_stories_from_file(file_name):
 
 
 def save_stories_to_file(file_name, story_list):
+    """
+    Save data to a file.
+    @file_name: where to save
+    @story_list: list of records
+    """
     with open(file_name, "w") as file:
         for story in story_list:
             row = ";".join([str(story.id),
@@ -46,16 +55,25 @@ def save_stories_to_file(file_name, story_list):
 
 
 def get_new_id(story_list):
+    """
+    Looks for the maximum of ids in story_list, and return a greater one by 1
+    """
     id_list = [x.id for x in story_list]
     return max(id_list) + 1
 
 
 def get_index_from_id(story_list, id):
+    """
+    Return the index of a record by its id
+    """
     for i in range(len(story_list)):
         if story_list[i].id == id:
             break
     return i
 
+
+# List webpage
+# Lists all the stored stories
 
 @app.route("/")
 @app.route("/list/")
@@ -65,6 +83,10 @@ def list_of_stories():
         return render_template("list.html", story_list=story_list)
 
 
+# Delete a story
+# No webpage is showed
+# Redirects to list page
+
 @app.route("/story/del@id=<int:id>")
 def delete_story(id):
     story_list = load_stories_from_file("stories.csv")
@@ -72,6 +94,9 @@ def delete_story(id):
     save_stories_to_file("stories.csv", story_list)
     return redirect("/", code=302)
 
+
+# Add new story page
+# When new story is requested, handles the form for it
 
 @app.route("/story/", methods=["GET", "POST"])
 def add_new_story():
@@ -93,14 +118,17 @@ def add_new_story():
         return render_template("form.html", title="Add new Story", story=story)
 
 
+# Edit story page
+# When edit story is requested, handles the form for it
+
 @app.route("/story/<int:story_id>", methods=["GET", "POST"])
 def edit_story(story_id):
     if request.method == "POST":
         story_list = load_stories_from_file("stories.csv")
         i = get_index_from_id(story_list, story_id)
         story_list[i].title = request.form["story.title"]
-        story_list[i].long_desc = request.form["story.long_desc"]
-        story_list[i].accept = request.form["story.accept"]
+        story_list[i].long_desc = request.form["story.long_desc"].replace("\n", " ")
+        story_list[i].accept = request.form["story.accept"].replace("\n", " ")
         story_list[i].value = request.form["story.value"]
         story_list[i].estim = request.form["story.estim"]
         story_list[i].status = request.form["story.status"]
